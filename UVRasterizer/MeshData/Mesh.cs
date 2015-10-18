7c2bc167373c39cf6233c1ecee089f224042c62b
Fizzly <mrpeters3d@gmail.com>
2015-10-18 14:12:48 +0200

    Obj loader from http://wiki.unity3d.com/index.php?title=ObjImporter

---
+++ UVRasterizer/MeshData/Mesh.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UVRasterizer.MeshData
+{
+    class Mesh
+    {
+        public Vector3[] vertices;
+        public Vector2[] uv;
+        public Vector3[] normals;
+        public int[] triangles;
+    }
+}
---
+++ UVRasterizer/MeshData/Vector2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UVRasterizer.MeshData
+{
+    class Vector2
+    {
+        public float x;
+        public float y;
+
+        public Vector2()
+        {
+
+        }
+
+        public Vector2(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
